b85634dc9f3289f26c3fd2f6d0af7a9f91646eb0
SteveSnow <Steven.Snow@outlook.com>
2016-03-06 15:14:33 -0500

    added ability to delete posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,66 +1,83 @@
 class UsersController < ApplicationController
 
-	def create
-		user = User.create(user_params)
-		session[:user_id] = user.id
-	    redirect_to user_path user
-	end
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to user_path user
+  end
 
-	def new
+  def new
 
-	end
+  end
 
-	def edit
-		@user=get_user
-	end
+  def edit
+    @user=get_user
+  end
 
-	def show
-		@user=get_user
-		@posts=[]
-		@users=User.all
+  def show
+    @user=get_user
+    @posts=[]
+    @users=User.all
 
-		@user.user_follows.each do |d|
-			User.find(d.follows_id).posts.each do |u|
-				@posts.push(u)
-			end
-		end
-	end
+    @user.user_follows.each do |d|
+      User.find(d.follows_id).posts.each do |u|
+        @posts.push(u)
+      end
+    end
+  end
 
-	def destroy
+  def destroy
 
-	end
+  end
 
-	def update
-		user=User.update(user_edit_params)
-	end
+  def update
+    user=User.update(user_edit_params)
+  end
 
-	def get_user
-		return User.find_by_id(session[:user_id])
-	end
+  def get_user
+    return User.find_by_id(session[:user_id])
+  end
 
-	def check_session
-		return session[:id]==params[:id]
-	end
+  def check_session
+    return session[:id]==params[:id]
+  end
 
-	def add_post
-		@user=get_user
-		if !params[:message].nil?
-			# binding.pry
-			@user.posts.create(message: params[:message])
-		end
+  def add_post
+    @user=get_user
+    if !params[:message].nil?
+      # binding.pry
+      @user.posts.create(message: params[:message])
+    end
 
-		redirect_to user_path session[:user_id]
-	end
+    redirect_to user_path session[:user_id]
+  end
 
-	private
+  def follow
+    user=get_user
+    user.follow_user(params[:id])
+    redirect_to user_path session[:user_id]
+  end
 
-	def user_params
-		params.require(:user).permit(:first_name,:last_name,:email,:password)
-	end
-	def user_edit_params
-		params.require(:user).permit(:first_name,:last_name,:email)
-	end
+  def unfollow
+    user=get_user
+    user.unfollow_user(params[:id])
+    redirect_to user_path session[:user_id]
+  end
+
+  def delete_post
+    user=get_user
+    if params[:post]
+      user.posts.delete(params[:post])
+    end
+    redirect_to user_path session[:user_id]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:email,:password)
+  end
+  def user_edit_params
+    params.require(:user).permit(:first_name,:last_name,:email)
+  end
 end
-
-
-
