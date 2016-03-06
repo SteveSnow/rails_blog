29537a3e3ac691646bdfc452bf2d95fb2252f331
Sam Krause <samuelnkrause@gmail.com>
2016-03-06 10:09:35 -0500

    trying to add javascript to comment box only appears on a click.  still a work in progress.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
     post_user_id = Relationship.where(follower_id: session[:user_id]).pluck(:follows_id)
     @posts = Post.where(user_id: post_user_id)
   end
+
+
 end
 
 
